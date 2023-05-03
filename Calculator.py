8acc1306a3a8a8dd4241598a3d3aff61b4a360aa
arpan285 <arpanchakraborty451@gmal.com>
2023-05-03 13:17:32 +0530

    FistCommit

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+class Cal:
+    def __init__(self, a,b):
+        print("This is parametrized constructor")
+        self.x = a
+        self.y = b
+    def addition(self):
+        print("ANS=",self.x+self.y)
+
+    def subs(self):
+        print("ANS=",self.x-self.y)
+
+num = int(input("Enter a number: "))
+num1 = int(input("Enter another number: "))
+
+obj = Cal(num,num1)
+obj.addition()
+obj.subs()
